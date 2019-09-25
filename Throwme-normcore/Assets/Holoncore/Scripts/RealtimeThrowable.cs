1560610f9db70ede01995f7ca05071214fc387a4
Maf'j Alvarez <jay@rootinteractive.com>
2019-09-25 14:54:16 +0100

    added script for requesting ownership on grab

---
+++ Throwme-normcore/Assets/Holoncore/Scripts/RealtimeThrowable.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine; 
+using Normal.Realtime;
+
+    public class RealtimeThrowable : MonoBehaviour
+    {
+
+        private RealtimeTransform bob;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            bob = gameObject.GetComponent<RealtimeTransform>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+
+        public void Grabbed()
+        {
+            bob.RequestOwnership();
+        }
+        public void Released()
+        {
+
+        }
+    }
+
